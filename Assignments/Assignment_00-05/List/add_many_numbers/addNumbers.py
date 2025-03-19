7d7c4e174e4e3f826f03bb859c6eeb8693b8875e
Syed Hamza Ali <161072811+Syed-Hamza-Ali-8@users.noreply.github.com>
2025-03-20 02:26:32 +0500

    Add many numbers

---
+++ Assignments/Assignment_00-05/List/add_many_numbers/addNumbers.py
@@ -0,0 +1,15 @@
+def add_many_numbers(numbers)-> int:
+
+    total: int = 0
+    for number in numbers:
+        total += number
+
+    return total
+
+def main():
+    numbers: list[int] = [1, 2, 3, 4, 5, 6]
+    sum_of_numbers: int = add_many_numbers(numbers)
+    print(sum_of_numbers)
+    
+if __name__ == '__main__':
+    main()
