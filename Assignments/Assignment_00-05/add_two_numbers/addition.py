c360213d8f222062e394e10568c3f95a42df60c5
Syed Hamza Ali <161072811+Syed-Hamza-Ali-8@users.noreply.github.com>
2025-03-19 16:47:07 +0500

    Basic addition with two numbers

---
+++ Assignments/Assignment_00-05/add_two_numbers/addition.py
@@ -0,0 +1,8 @@
+def main():
+    num1: float = float(input("Enter first Num: "))
+    num2: float = float(input("Enter second Num: "))
+    res = num1 + num2
+    print(num1, "+", num2, "=", res)
+
+if __name__ == '__main__':
+    main()
