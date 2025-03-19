ba95d17fb162489342d56ee31e4270d5f9539353
Syed Hamza Ali <161072811+Syed-Hamza-Ali-8@users.noreply.github.com>
2025-03-19 16:58:10 +0500

    basic animal name display

---
+++ Assignments/Assignment_00-05/favourite_animal/animal.py
@@ -0,0 +1,8 @@
+def main():
+ animal : str = input("What is your favourite animal ? ")
+ if animal:
+  print("My Favourite Animal is",animal)
+ else:
+  print("My Favourite Animal is ME")
+if __name__ == '__main__':
+    main()
